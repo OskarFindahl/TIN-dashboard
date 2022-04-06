5577f05f6df5cbb6fb6b88c0ff154651371c948b
Oskar Findahl <oskar_findahl@hotmail.com>
2022-04-06 15:13:35 +0200

    add spider filter

--- backend/zyte/views.py
+++ backend/zyte/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
+
 from .serializers import JobDataSerializer,  SpiderSerializer
 from .models import JobData, Spider
 from .mixins import ManyOnListMixin, NoReturnCreateModelMixin
@@ -8,5 +10,7 @@ class SpiderViewSet(ManyOnListMixin, viewsets.ModelViewSet):
     serializer_class = SpiderSerializer
 
 class JobDataViewSet(ManyOnListMixin, NoReturnCreateModelMixin, viewsets.ModelViewSet):
-    queryset = JobData.objects.all()
+    queryset = JobData.objects.all().order_by('date')
     serializer_class = JobDataSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ("spider", )
