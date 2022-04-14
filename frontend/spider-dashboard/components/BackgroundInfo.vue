eff804b22d9a418d98b092b4b497eb7c6a382c28
Oskar Findahl <oskar_findahl@hotmail.com>
2022-04-14 11:05:17 +0200

    add background info

---
+++ frontend/spider-dashboard/components/BackgroundInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <div class="text-wrapper">
+      <h1>TIN-Dashboard</h1>
+      <p>Service Status</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.wrapper {
+  background-color: #131a26;
+  width: 100%;
+  height: 200px;
+  position: relative;
+  top: 0px;
+  z-index: -10;
+}
+
+p {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.text-wrapper {
+  padding: 60px 190px 0px 190px;
+  display: flex;
+  color: white;
+  justify-content: space-between;
+  font-family: "Roboto", sans-serif;
+}
+</style>
