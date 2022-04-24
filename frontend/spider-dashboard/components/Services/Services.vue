4c3f78a761f99029a2f2a6315e19609077568462
Oskar Findahl <oskar_findahl@hotmail.com>
2022-04-24 10:21:27 +0200

    add service component for fetching data

---
+++ frontend/spider-dashboard/components/Services/Services.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Services</h1>
+    <div class="wrapper">
+      <template :key="spider.id" v-for="spider in spiders">
+        <ServicesRow :spider="spider" />
+      </template>
+    </div>
+  </div>
+</template>
+
+
+
+
+<script setup>
+const { data } = await useFetch("http://localhost:8000/zyte/spiders");
+let spiders = data._value;
+</script>
+
+<style scoped>
+.wrapper {
+  background: #fff;
+  padding: 60px;
+  box-shadow: 0 20px 60px rgb(0 0 0 / 10%);
+  border-radius: 6px;
+  color: #131a26;
+  margin: 20px 185px;
+}
+
+h1 {
+  margin: 20px 185px;
+  font-family: "Roboto", sans-serif;
+}
+</style>
