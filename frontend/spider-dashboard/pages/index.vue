95a4e5d7c93aa85ee7493f2090076f69459e60bf
Oskar Findahl <oskar_findahl@hotmail.com>
2022-04-24 10:21:34 +0200

    add component index

---
+++ frontend/spider-dashboard/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout name="custom">
+    <BackgroundInfo />
+    <OverallStatus />
+
+    <Services />
+  </NuxtLayout>
+</template>
+
+
+<script>
+// This will also work in `<script setup>`
+
+definePageMeta({
+  layout: false,
+});
+</script>
