1fc81a3edb92c97ee88fca5965dec0ef6072d7b5
Oskar Findahl <oskar_findahl@hotmail.com>
2022-04-27 22:20:05 +0200

    add typescript

--- frontend/spider-dashboard/components/Services/Services.vue
+++ frontend/spider-dashboard/components/Services/Services.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <h1>Services</h1>
+    
     <div class="wrapper">
-      <template :key="spider.id" v-for="spider in spiders">
-        <ServicesRow :spider="spider" />
-      </template>
+      <div class="card">
+        <h1 class="mb-5">Services</h1>
+        <template :key="spider.id" v-for="spider in spiders">
+          <ServicesRow :spider="spider" />
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -12,23 +15,16 @@
 
 
 
-<script setup>
-const { data } = await useFetch("http://localhost:8000/zyte/spiders");
-let spiders = data._value;
+<script lang="ts" setup>
+interface Spider {
+  id: string;
+}
+
+interface SpidersFetch{
+  data: Spider[];
+}
+
+const { data } = await useFetch<SpidersFetch>("http://localhost:8000/zyte/spiders");
+
+let spiders = data.value;
 </script>
-
-<style scoped>
-.wrapper {
-  background: #fff;
-  padding: 60px;
-  box-shadow: 0 20px 60px rgb(0 0 0 / 10%);
-  border-radius: 6px;
-  color: #131a26;
-  margin: 20px 185px;
-}
-
-h1 {
-  margin: 20px 185px;
-  font-family: "Roboto", sans-serif;
-}
-</style>
